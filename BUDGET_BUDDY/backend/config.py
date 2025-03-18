5f870aa0160889e24bcb2b3c1f3913557f9c9e4d
Dziri <redha.aggoun@laplateforme.io>
2025-03-18 16:00:31 +0100

    added config.py

---
+++ BUDGET_BUDDY/backend/config.py
@@ -0,0 +1,10 @@
+# ✅ Security Settings
+SECRET_KEY = "21091976"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# ✅ Database Settings
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "21091976"
+DB_NAME = "budgetbuddy"
